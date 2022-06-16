8d125f1ee049b7408242c8bf0aa301cf823c19c2
Igneeles <69846931+Igneele91@users.noreply.github.com>
2022-06-16 20:28:41 +0200

    Uploud filte Test Variabeln
    How it works and have i understand it

---
+++ dart_grundlagen/2_variablen.dart
@@ -0,0 +1,29 @@
+
+void main( ) 
+{
+  int alter = 32;
+
+  late int alter_2;
+
+  alter_2 = 48;
+
+  print(alter);
+
+  print(alter_2);
+
+  alter_2 = 50;
+
+  print(alter_2);
+
+  
+
+}
+
+
+
+
+
+
+
+
+
