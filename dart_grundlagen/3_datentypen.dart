91bf551bc257a5495c67fe8dc5e5fb96e061db93
Igneeles <69846931+Igneele91@users.noreply.github.com>
2022-06-17 00:51:23 +0200

    daten typ string
    und print (=9)

---
+++ dart_grundlagen/3_datentypen.dart
@@ -0,0 +1,35 @@
+
+void main( )
+
+{
+  int alter = 32;   //!      intiger = itteger
+
+  double meinDouble = 4.4;  //?  double
+  
+  bool meinBool = false;   // bool
+
+
+  // ###########################################
+
+  String meinString = "DerBall";
+
+  String zweitenString = "ist rot.";
+
+  String verkettet = meinString + zweitenString;   
+
+  print (verkettet);
+
+  print (meinString + zweitenString);                                                         
+
+  print (9);
+
+
+}
+
+//          das ist ein grüner Komentar
+//?         das ist ein blauer Kommentar
+//!         das ist ein roter Kommentar
+//*         das ist ein hellgrüner Kommentar
+//todo      das ist ein hellgrüner Kommentar]
+
+
