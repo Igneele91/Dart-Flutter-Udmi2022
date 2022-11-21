7e7121f17984894e625d8c0d7c002078b7b91557
Igneele <69846931+Igneele91@users.noreply.github.com>
2022-11-21 20:58:58 +0100

    if abfrage and switch case

---
+++ dart_grundlagen/7_switch_case.dart
@@ -0,0 +1,20 @@
+
+void main (){
+
+    int alter = 23;
+    String name = "Hurry";
+
+    print("vor dem switch");
+
+    switch (name){
+      case "Peter":
+        print("ich bin Peter");
+        break;
+      case "Hans":
+        print("ich bin Hans");
+        break;
+      default:
+        print("keine ahnung");
+    }
+     print("nach dem switch");
+}
