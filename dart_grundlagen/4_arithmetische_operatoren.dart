a8c50d27d49c09a9fcb63d9ff0afb4a4ef589344
Igneele <69846931+Igneele91@users.noreply.github.com>
2022-11-21 20:23:22 +0100

    operation and list and maps

---
+++ dart_grundlagen/4_arithmetische_operatoren.dart
@@ -0,0 +1,22 @@
+
+void main (){
+  double number = (42 + 4) /2 ;
+  print(number);
+
+  //? NR 2
+
+  double number1 = (12 / 4) ;
+  print(number1);
+
+  //? NR 3
+
+  double number2 = (42 + 4);
+  print(number2);
+
+  //? NR 4
+
+  int number4 = (42 - 4);
+  print(number4);
+
+
+}
---
+++ dart_grundlagen/5_lists_maps.dart
@@ -0,0 +1,36 @@
+
+void main() {
+  //! Liste Kategorie
+  List <int> liste = [1,2,3,];
+  
+  print(liste);
+  
+  print(liste[1]);
+
+  print(liste.first);
+
+  print(liste.length);
+
+  
+  print(liste.isEmpty);
+
+  liste.add(3);
+
+  print(liste);
+
+  //! Map Kategorie        =    unterschied sind die geschweiften klammern
+
+  String String1 = "------ Map ----------";
+  print(String1);
+
+  Map<String,String> map = {"key1":"value1", "key2":"value2",};
+
+  print(map.length);
+
+  print(map.keys);
+
+  print(map["key2"]);
+
+  
+}
+
