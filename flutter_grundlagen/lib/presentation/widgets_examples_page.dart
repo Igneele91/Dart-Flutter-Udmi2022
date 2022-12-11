d572be8555cb6b7b0d07f94ac046295a2f46ed24
Igneele <69846931+Igneele91@users.noreply.github.com>
2022-12-11 19:40:12 +0100

    code learn comit1 t

---
+++ flutter_grundlagen/lib/presentation/widgets_examples_page.dart
@@ -0,0 +1,20 @@
+
+
+  import 'package:flutter/material.dart';
+
+  class WidgetsExamplesPage extends StatelessWidget {
+    const WidgetsExamplesPage({Key? key}) : super(key: key);
+  
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+        backgroundcolor: Colors.grey[900],
+          appBar: AppBar(
+            title: Text ("my app FF"),
+          ), // AppBar
+          body: Placeholder()
+
+      ); // Scaffold
+    }
+  }
+  
