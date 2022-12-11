fd7498b6714a1b8254f472b63063b09301519af3
Igneele <69846931+Igneele91@users.noreply.github.com>
2022-12-11 19:55:57 +0100

    build it correct flut1

--- flutter_grundlagen/lib/main.dart
+++ flutter_grundlagen/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_grundlagen/presentation/widgets_examples_page.dart';
+import 'package:flutter_grundlagen/presentation/widgets_examples/widgets_examples_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,9 +10,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: WidgetsExamplesPage()
-    );
+    return MaterialApp(home: WidgetsExamplesPage());
   }
 }
-
--- flutter_grundlagen/lib/presentation/widgets_examples/widgets_examples_page.dart
+++ flutter_grundlagen/lib/presentation/widgets_examples/widgets_examples_page.dart
@@ -1,20 +1,15 @@
+import 'package:flutter/material.dart';
 
+class WidgetsExamplesPage extends StatelessWidget {
+  const WidgetsExamplesPage({Key? key}) : super(key: key);
 
-  import 'package:flutter/material.dart';
-
-  class WidgetsExamplesPage extends StatelessWidget {
-    const WidgetsExamplesPage({Key? key}) : super(key: key);
-  
-    @override
-    Widget build(BuildContext context) {
-      return Scaffold(
-        backgroundcolor: Colors.grey[900],
-          appBar: AppBar(
-            title: Text ("my app FF"),
-          ), // AppBar
-          body: Placeholder()
-
-      ); // Scaffold
-    }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        // backgroundcolor: Colors.grey[900],
+        appBar: AppBar(
+          title: Text("my app FF"),
+        ), // AppBar
+        body: Placeholder()); // Scaffold
   }
-  
+}
