962162cfa03a0fbf1fe0ba42d47d81bc10aa9b02
Igneele <69846931+Igneele91@users.noreply.github.com>
2022-12-12 01:18:58 +0100

    box in box after with3deffect

--- flutter_grundlagen/lib/presentation/widgets_examples/widgets/container_text_example.dart
+++ flutter_grundlagen/lib/presentation/widgets_examples/widgets/container_text_example.dart
@@ -6,10 +6,26 @@ class ContainerTextExample extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-        child: Container(
-      height: 200,
-      width: 200,
-      color: Colors.grey,
-    ));
+      child: Container(
+        height: 180,
+        width: 300,
+        decoration: BoxDecoration(
+            color: Colors.grey,
+            border: Border.all(
+              color: Colors.black,
+              width: 4,
+            ),
+            borderRadius: BorderRadius.circular(16)),
+        child: Center(
+          child: Container(
+            height: 100,
+            width: 200,
+            decoration: BoxDecoration(
+                color: Colors.blue[200],
+                borderRadius: BorderRadius.circular(10)),
+          ),
+        ),
+      ), // das Komma für neue line merken
+    );
   }
 }
