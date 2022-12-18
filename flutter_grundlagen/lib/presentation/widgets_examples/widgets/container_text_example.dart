631787a80c12c030a5bc7075913ed0aeff08924a
Igneele <69846931+Igneele91@users.noreply.github.com>
2022-12-18 01:50:42 +0100

    changes of all bitt framepake1234314

--- flutter_grundlagen/lib/presentation/widgets_examples/widgets/container_text_example.dart
+++ flutter_grundlagen/lib/presentation/widgets_examples/widgets/container_text_example.dart
@@ -5,44 +5,42 @@ class ContainerTextExample extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        // Container grau1,
-        height: 180,
-        width: 300,
-        decoration: BoxDecoration(
-            color: Colors.grey,
-            border: Border.all(
-              color: Colors.black,
-              width: 4,
-            ),
-            borderRadius: BorderRadius.circular(16)),
-        child: Center(
-          child: Material(
-            borderRadius: BorderRadius.circular(10),
-            elevation: 8,
-            child: Container(
-              // Container blau2
-              alignment: Alignment.center, // Schatten3Deffektmit rand schwarz
-              //padding: const EdgeInsets.only(bottom: 20),
-              height: 100,
-              width: 200,
-              decoration: BoxDecoration(
-                  color: Colors.blue[200],
-                  borderRadius: BorderRadius.circular(10)),
-              child: const Text(
-                // Text davon
-                "Text Example",
-                style: TextStyle(
-                  fontStyle: FontStyle.italic,
-                  fontFamily: "Rubrik",
-                  fontSize: 20,
-                ),
+    return Container(
+      // Container grau1,
+      height: 180,
+      width: 300,
+      decoration: BoxDecoration(
+          color: Colors.grey,
+          border: Border.all(
+            color: Colors.black,
+            width: 4,
+          ),
+          borderRadius: BorderRadius.circular(16)),
+      child: Center(
+        child: Material(
+          borderRadius: BorderRadius.circular(10),
+          elevation: 8,
+          child: Container(
+            // Container blau2
+            alignment: Alignment.center, // Schatten3Deffektmit rand schwarz
+            //padding: const EdgeInsets.only(bottom: 20),
+            height: 100,
+            width: 200,
+            decoration: BoxDecoration(
+                color: Colors.blue[200],
+                borderRadius: BorderRadius.circular(10)),
+            child: const Text(
+              // Text davon
+              "Text Example",
+              style: TextStyle(
+                fontStyle: FontStyle.italic,
+                fontFamily: "Rubrik",
+                fontSize: 20,
               ),
             ),
           ),
         ),
-      ), // das Komma für neue line merken
+      ),
     );
   }
 }
--- flutter_grundlagen/lib/presentation/widgets_examples/widgets_examples_page.dart
+++ flutter_grundlagen/lib/presentation/widgets_examples/widgets_examples_page.dart
@@ -13,10 +13,105 @@ class WidgetsExamplesPage extends StatelessWidget {
             Icons.home,
             size: 30,
           ),
-          centerTitle: true, // mitte legen titel
-          backgroundColor: Colors.orange[700],
+          centerTitle: true,
+          backgroundColor: Colors.blue,
           title: const Text("Widgets Examples"),
-        ), // AppBar
-        body: const ContainerTextExample()); // Scaffold
+        ),
+        body: SingleChildScrollView(
+          physics: const BouncingScrollPhysics(),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const ContainerTextExample(),
+                const SizedBox(
+                  height: 10,
+                ),
+                /*  const RowExpandedExample(),
+                const SizedBox(
+                  height: 30,
+                ),
+                const ProfilePicture(),
+                const SizedBox(
+                  height: 30,
+                ),
+                const RectImage(),
+                const SizedBox(
+                  height: 30,
+                ),
+                const MediaQueryExample(),
+                const SizedBox(
+                  height: 30,
+                ),
+                const PageViewExample(),
+                const SizedBox(
+                  height: 30,
+                ),
+                IconButton(
+                    splashColor: Colors.blue,
+                    onPressed: () {
+                      // ignore: avoid_print
+                      print("Icon Button pressed!");
+                    },
+                    icon: const Icon(
+                      Icons.home,
+                      color: Colors.white,
+                    )),
+                const SizedBox(
+                  height: 30,
+                ),
+                TextButton(
+                    onPressed: () {
+                      // ignore: avoid_print
+                      print("Text Button Pressed");
+                    },
+                    child: Container(
+                        color: Colors.grey,
+                        child: const Text(
+                          "Text Button",
+                          style: TextStyle(color: Colors.white),
+                        ))),
+                const SizedBox(
+                  height: 30,  
+                ),*/
+                CustomButton(
+                  buttonColor: Colors.blue[200]!,
+                  onPressed: () {
+                    Navigator.of(context).pushNamed("/screen2");
+                  },
+                  text: 'Got to Screen 2',
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                CustomButton(
+                  buttonColor: Colors.green[200]!,
+                  onPressed: () {
+                    Navigator.of(context).pushNamed("/screen1");
+                  },
+                  text: 'Go to Screen 1',
+                ),
+                const SizedBox(
+                  width: 15,
+                ),
+                Switch(
+                    value: Provider.of<ThemeService>(context).isDarkModeOn,
+                    onChanged: (value) {
+                      Provider.of<ThemeService>(context, listen: false)
+                          .toggleTheme();
+                    })
+              ],
+            ),
+          ),
+        ));
   }
 }
+
+class Provider {}
+
+CustomButton(
+    {required Color buttonColor,
+    required Null Function() onPressed,
+    required String text}) {}
