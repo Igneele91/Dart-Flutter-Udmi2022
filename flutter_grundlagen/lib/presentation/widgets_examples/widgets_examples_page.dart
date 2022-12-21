77bdd63c44000d47ae1629d295e3bd75a3e98d1a
Igneele <69846931+Igneele91@users.noreply.github.com>
2022-12-21 18:13:43 +0100

    vid 37 change 45

--- flutter_grundlagen/lib/presentation/widgets_examples/widgets_examples_page.dart
+++ flutter_grundlagen/lib/presentation/widgets_examples/widgets_examples_page.dart
@@ -34,12 +34,20 @@ class WidgetsExamplesPage extends StatelessWidget {
               SizedBox(
                 height: 10,
               ),
-              ProfilePicutre()
+              ProfilePicutre(),
               SizedBox(
                 height: 10,
               ),
+              Container(
+                width: 200,
+                height: 100,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: Image.asset("assets/images/Igneele-fox-right-side.jpg"),
+                ),
+              )
             ],
-          ),
+          ), s
         ),
       ),
     );
