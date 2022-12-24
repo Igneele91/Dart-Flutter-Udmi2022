886460e5a6e528a250c0443d6df641c6db5c555b
Igneele <69846931+Igneele91@users.noreply.github.com>
2022-12-24 20:28:01 +0100

    vid 38 scroll effect final version finish comit 2

---
+++ flutter_grundlagen/lib/presentation/widgets_examples/widgets/media_query_example.dart
@@ -0,0 +1,19 @@
+
+import 'package:flutter/material.dart';
+
+
+
+class MediaQueryExample extends StatelessWidget {
+  const MediaQueryExample({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
+  return Container(
+    height: size.height * 0.5,
+    width: size.width * 0.8,
+    color: Colors.green,
+  );
+  }
+}
--- flutter_grundlagen/lib/presentation/widgets_examples/widgets_examples_page.dart
+++ flutter_grundlagen/lib/presentation/widgets_examples/widgets_examples_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_grundlagen/presentation/widgets_examples/widgets/container_text_example.dart';
+import 'package:flutter_grundlagen/presentation/widgets_examples/widgets/media_query_example.dart';
 import 'package:flutter_grundlagen/presentation/widgets_examples/widgets/profile_picture.dart';
 import 'package:flutter_grundlagen/presentation/widgets_examples/widgets/rectangular_image.dart';
 import 'package:flutter_grundlagen/presentation/widgets_examples/widgets/row_expanded_example.dart';
@@ -17,10 +18,11 @@ class WidgetsExamplesPage extends StatelessWidget {
           size: 30,
         ),
         centerTitle: true,
-        backgroundColor: Colors.pink,
+        backgroundColor: Colors.pink[200],
         title: const Text("Widgets Examples"),
       ),
       body: SingleChildScrollView(
+        physics: const BouncingScrollPhysics(), // Nimmt beim Scrollen den effekt weg blau
         child: Padding(
           padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
           child: Column(
@@ -33,13 +35,17 @@ class WidgetsExamplesPage extends StatelessWidget {
               ),
               RowExpandedExample(),
               SizedBox(
-                height: 10,
+                height: 30,
               ),
               ProfilePicture(),
               SizedBox(
-                height: 10,
+                height: 30,
               ),
               RectImage(),
+              SizedBox(
+                height: 30,
+              ),
+              MediaQueryExample(),
             ],
           ),
         ),
