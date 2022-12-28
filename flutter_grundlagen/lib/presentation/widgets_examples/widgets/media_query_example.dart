3f6cdaf376b93f4050c81f437cbbaba93788cd4b
Igneele <69846931+Igneele91@users.noreply.github.com>
2022-12-28 23:14:21 +0100

    vid 39 finish comit 1 with change

--- flutter_grundlagen/lib/presentation/widgets_examples/widgets/media_query_example.dart
+++ flutter_grundlagen/lib/presentation/widgets_examples/widgets/media_query_example.dart
@@ -8,7 +8,7 @@ class MediaQueryExample extends StatelessWidget {
     final size = MediaQuery.of(context).size;
 
     return Container(
-      height: size.height * 0.5,
+      height: size.height * 0.2, // höhe von der grüne box 0.1 video
       width: size.width * 0.8,
       color: Colors.green,
     );
--- flutter_grundlagen/lib/presentation/widgets_examples/widgets/page_view_exmaple.dart
+++ flutter_grundlagen/lib/presentation/widgets_examples/widgets/page_view_exmaple.dart
@@ -7,39 +7,80 @@ class PageViewExample extends StatelessWidget {
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
     return SizedBox(
-      height: size.height * 0.3,
+      height: size.height * 0.2, // Box Grosse grau
       child: PageView(
         controller: PageController(viewportFraction: 0.95),
         children: [
-          Padding(
+          Padding(  // Container 1
             padding: const EdgeInsets.only(right: 8.0),
-            child: Container(
-              height: size.height * 0.3,
-              width: size.width * 0.85,
-              color: Colors.blue,
+            child: SingelPage(
+              size: size,
+              title: 'Katze',
+              text: 'Katze frisst die Maus',
             ),
           ),
 
-          Padding(
+          Padding( //Container 2
             padding: const EdgeInsets.only(right: 8.0),
-            child: Container(
-              height: size.height * 0.3,
-              width: size.width * 0.85,
-              color: Colors.yellow,
+            child: SingelPage(
+              size: size,
+              title: 'Jaguar jagt einen Hase',
+              text: 'Der Hase schmekte dem Jaguar',
             ),
           ),
 
-          Padding(
+          Padding( // Container 3
             padding: const EdgeInsets.only(right: 8.0),
-            child: Container(
-              height: size.height * 0.3,
-              width: size.width * 0.85,
-              color: Colors.purple,
+            child: SingelPage(
+              size: size,
+              title: 'Hunde sind Doff',
+              text: 'Das Krokodil liegt im Fluss sdnfjnsjkdfnjksnfjknbsdfjk jsdfnjsnfdjsn',
             ),
           ),
 
           // Controller,
         ],
+      ),
+    );
+  }
+}
+
+class SingelPage extends StatelessWidget {
+  final Size size;
+  final String title;
+  final String text;
+  const SingelPage(
+      {Key? key, required this.size, required this.title, required this.text})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: size.height * 0.2,
+      width: size.width * 0.85,
+      decoration: BoxDecoration(
+          color: Colors.grey[700], borderRadius: BorderRadius.circular(15)),
+      child: Padding(
+        padding: const EdgeInsets.all(30),
+        child: Column(
+          children: [
+            Text(
+              title,
+              style: const TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Text(
+              text,
+              style: const TextStyle(color: Colors.white, fontSize: 15),
+              textAlign: TextAlign.center,
+            ),
+          ],
+        ),
       ),
     );
   }
